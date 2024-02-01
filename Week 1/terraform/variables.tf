b09567d1d7eb267b090edc6063535a88b05ec274
DrewDimas <132039422+DrewDimas@users.noreply.github.com>
2024-02-01 01:05:56 +0000

    Finished module

---
+++ Week 1/terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "credentials" {
+  description = "Project Creds"
+  default     = file("/workspaces/DataCamp2024/Week 1/terraform/keys/my-creds.json")
+}
+
+variable "project" {
+  description = "Project Name"
+  default     = "datacamp2024-412820"
+}
+
+variable "region" {
+  description = "Project Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery dataset name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "Storage Bucket Name"
+  default     = "datacamp2024-412820-terrabucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket storage class"
+  default     = "STANDARD"
+}
