0639cce429c62c0b8e71848ec087cba77b261d37
DrewDimas <132039422+DrewDimas@users.noreply.github.com>
2024-03-09 05:42:40 +0000

    new file:   Week 1/mage-zoomcamp/magic-zoomcamp/.gitignore 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/__init__.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/charts/__init__.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/custom/__init__.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/data_exporters/__init__.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/data_exporters/export_titanic_clean.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/data_loaders/__init__.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/data_loaders/load_titanic.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/dbt/profiles.yml 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/extensions/__init__.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/interactions/__init__.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/io_config.yaml 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/metadata.yaml 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/pipelines/__init__.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/pipelines/example_pipeline/__init__.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/pipelines/example_pipeline/metadata.yaml 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/requirements.txt 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/scratchpads/__init__.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/transformers/__init__.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/transformers/fill_in_missing_values.py 	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/utils/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/.gitignore
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/charts/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/custom/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/data_exporters/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/data_exporters/export_titanic_clean.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/data_loaders/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/data_loaders/load_titanic.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/dbt/profiles.yml
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/extensions/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/interactions/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/io_config.yaml
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/metadata.yaml
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/pipelines/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/pipelines/example_pipeline/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/pipelines/example_pipeline/metadata.yaml
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/requirements.txt
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/scratchpads/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/transformers/__init__.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/transformers/fill_in_missing_values.py
    	new file:   Week 1/mage-zoomcamp/magic-zoomcamp/utils/__init__.py

---
+++ Week 1/mage-zoomcamp/magic-zoomcamp/data_exporters/export_titanic_clean.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:44eff674daa2e3946c823975d9c18e84e004eb5303df7a19f8823bed98d0ffc8
+size 460
---
+++ Week 1/mage-zoomcamp/magic-zoomcamp/transformers/fill_in_missing_values.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ae916cc5e1169e1f4581b99fbd3b3b682d5bdea29c6477b8005564a8072b39cd
+size 1320
